7d312c302233cb2df5543a2d0df39f0a9a618ab1
Kauã Tavares <105657003+Kauadt@users.noreply.github.com>
2024-05-19 19:00:03 -0300

    Update colors.js
    remove colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -12,28 +12,4 @@ export default {
   mainGreen: '#64D196',
   brighterBlue: '#002367',
   white: '#fff',
-
-
-
-
-
-
-
-
-
-  green: '#32B768',
-  green_dark: '#2B7A4B',
-  green_light: '#DAF2E4',
-
-  heading: '#52665A',
-  body_dark: '#738078',
-  body_light: '#AAB2AD',
-  shape: '#F0F0F0',
-  // white: '#FFFFFF',
-  gray: '#CFCFCF',
-
-  blue: '#3D7199',
-  blue_light: '#EBF6FF',
-
-  red: '#E83F5B',
 };
